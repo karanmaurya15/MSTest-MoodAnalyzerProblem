dfc29659f494954c1a7760327f8c9c2c1fa9f8e7
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-18 21:47:46 +0530

    Wellcome

---
+++ MSTest-MoodAnalyzerProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSTest_MoodAnalyzerProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Mood Analyzer Problem");
+        }
+    }
+}
